a868dae8b257c15e42cb1aa7a96c3d1e6fd502d7
goncalomotaribeiro <goncalomota.cr@gmail.com>
2021-04-12 17:45:44 +0100

    oo

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,8 @@ const config = {
     /* don't expose password or any sensitive info, done only for demo */
     // if environment variables are not defined, use default values
     HOST: process.env.DB_HOST || 'sql11.freemysqlhosting.net',
-    USER: process.env.DB_USER || 'sql11403738',
-    PASSWORD: process.env.DB_PASSWORD || '3yPUTQ7AzB',
-    DB: process.env.DB_NAME || 'sql11403738'
+    USER: process.env.DB_USER || 'sql11405175',
+    PASSWORD: process.env.DB_PASSWORD || '9296dLTqBi',
+    DB: process.env.DB_NAME || 'sql11405175'
 };
 module.exports = config;
